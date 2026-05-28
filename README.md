3bd4c3354cefeb29fd3f9f330a306f363e74c093
Awwal Braimah <braimahawwal@gmail.com>
2026-05-28 02:19:06 +0100

    added README

---
+++ README.md
@@ -0,0 +1,11 @@
+# My First Project
+
+## Description
+just testing things out
+
+## How to run it
+put it in your termial
+
+## Technologies used
+- CPP
+- Git
---
+++ testing/testing/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  testing
+//
+//  Created by Awwal Braimah on 28/05/2026.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
